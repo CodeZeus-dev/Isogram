99f7eaf2e4419e0cb73271de2efc27104c36241e
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-10 00:52:13 +0000

    Updated the IsIsogram method so that it replaces hyphens in the string with spaces.

--- Isogram.cs
+++ Isogram.cs
@@ -6,7 +6,7 @@ public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        word = word.Replace(" ", String.Empty).ToLower();
+        word = word.Replace(" ", String.Empty).Replace("-", String.Empty).ToLower();
         char[] charsInWord = word.ToCharArray(0, word.Length);
 
         IEnumerable<char> uniqueChars = charsInWord.Distinct<char>();
