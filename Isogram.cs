d3336f7c2ccf4a7d2b502bf712dcbda80c039fa3
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-10 00:47:45 +0000

    Initial check-in of module Isogram and added basic infrastructure for returning true when count of unique chars equals word length

---
+++ Isogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        word = word.Replace(" ", String.Empty).ToLower();
+        char[] charsInWord = word.ToCharArray(0, word.Length);
+
+        IEnumerable<char> uniqueChars = charsInWord.Distinct<char>();
+
+        if (string.Join("", uniqueChars).Length == word.Length)
+        {
+            return true;
+        }
+
+        return false; 
+    }
+}
